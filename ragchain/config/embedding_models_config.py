6c7ba7eb5fbbfff81b67b4f8f3b459cd4f9f17ea
CoffeeCat1920 <H0neyCatCoffeeGo@proton.me>
2026-09-09 12:06:06 +0500

    gachar_machar

--- ragchain/config/embedding_models_config.py
+++ ragchain/config/embedding_models_config.py
@@ -1,10 +1,16 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field 
+from enum import Enum
 import json
+
+class TextEmbeddingModel(Enum):
+    HUGGINGFACE_EMBEDDING_MODEL = "huggingface_embedding_model" 
+    OLLAMA_EMBEDDING_MODEL = "ollama_embedding_model"
 
 @dataclass 
 class TextEmbeddingModelsList:
-    huggingface_embedding_model: str
-    ollama_embedding_model: str
+    text_embedding_models_list: dict = field(default_factory=dict)
+    def get(self, text_embedding_model : TextEmbeddingModel):
+        return self.text_embedding_models_list[text_embedding_model.value] 
 
 @dataclass
 class EmbeddingModelsConfig:
@@ -14,7 +20,7 @@ class EmbeddingModelsConfig:
 def LoadEmbeddingModelsConfigFromPath(path : str):
     with open(path) as f:
        data = json.load(f)
-       text_embedding_models_list = TextEmbeddingModelsList(**data["embedding_models_list"]["text_embedding_models_list"]) 
+       text_embedding_models_list = TextEmbeddingModelsList(**data["embedding_models_list"]) 
        current_text_embedding_mode = data["current_text_embedding_model"]
        embedding_models_list = EmbeddingModelsConfig(text_embedding_models_list=text_embedding_models_list, 
                                                    current_text_embedding_model=current_text_embedding_mode)
