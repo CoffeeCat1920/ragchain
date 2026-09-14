bb9f6f527d2a9b9f158eef2735d42ac6a023a187
CoffeeCat1920 <H0neyCatCoffeeGo@proton.me>
2026-09-14 11:16:16 +0500

    moving towards working with images

--- ragchain/__main__.py
+++ ragchain/__main__.py
@@ -3,6 +3,11 @@ from .vector_store.vector_store import VectorStore
 def main() -> None:
     _vector_store = VectorStore("chroma_db")
     print(_vector_store.injust("assets/green_lantern_lore.txt"))
+    results = _vector_store.retrieve("What is an green lantern?", 3)
+    for result in results:
+        print(result.page_content)
+        print("-----------------")
+    
 
 if __name__ == "__main__":
     main()
--- ragchain/vector_store/vector_store.py
+++ ragchain/vector_store/vector_store.py
@@ -3,6 +3,7 @@ from .text_clean import clean_text
 from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from ragchain.embedding_models.embedding_models_registry import EmbeddingRegistry
+from langchain_core.documents import Document
 
 class VectorStore():
     def __init__(self, path : str) -> None:
@@ -12,7 +13,11 @@ class VectorStore():
         self._text_splitter = RecursiveCharacterTextSplitter(
             chunk_size = 500,
             chunk_overlap = 100,
-                ) 
+            ) 
+        self._vectorstore = Chroma(
+            persist_directory=self._path,
+            embedding_function=self._embeddingRegistry.get_current_text_model(),
+        )
     
     def injust(self, path : str) -> None:
         dataType = self._typeChecker.check(path)
@@ -21,9 +26,9 @@ class VectorStore():
             with open(path, "r") as f:
                 cleaned_text = clean_text(f.read())
                 splits = self._text_splitter.split_text(text=cleaned_text) 
-                _ = Chroma.from_texts(
-                        texts=splits,
-                        embedding=self._embeddingRegistry.get_current_text_model(),
-                        persist_directory=self._path
-                        )
+                self._vectorstore.add_texts(splits)
 
+
+    def retrieve(self, query : str, k : int) -> list[Document]: 
+        results = self._vectorstore.similarity_search(query=query, k=k)
+        return results
