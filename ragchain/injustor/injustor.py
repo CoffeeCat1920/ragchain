6f014a78f62c20490c6b96c059e801cf969d8a08
CoffeeCat1920 <H0neyCatCoffeeGo@proton.me>
2026-09-14 13:31:17 +0500

    thing

---
+++ ragchain/injustor/injustor.py
@@ -0,0 +1,22 @@
+from langchain_core.embeddings import Embeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from ragchain.vector_store.text_clean import clean_text
+
+class Injustor():
+    def __init__(self, path : str, embModel : Embeddings):
+        self._text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 500,
+            chunk_overlap = 100,
+            ) 
+        self._vectorstore = Chroma(
+            persist_directory=path,
+            embedding_function=embModel,
+            ) 
+
+    def injust(self, path : str):
+        with open(path, "r") as f:
+            cleaned_text=clean_text(f.read())
+            splits=self._text_splitter.split_text(cleaned_text) 
+            self._vectorstore.add_texts(splits)
+
--- ragchain/vector_store/vector_store.py
+++ ragchain/vector_store/vector_store.py
@@ -1,34 +1,28 @@
 from .type_check import TypeChecker, FileType 
-from .text_clean import clean_text
-from langchain_chroma import Chroma
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from ragchain.embedding_models.embedding_models_registry import EmbeddingRegistry
 from langchain_core.documents import Document
+from ragchain.injustor.injustor import Injustor 
 
 class VectorStore():
     def __init__(self, path : str) -> None:
         self._path = path 
         self._typeChecker = TypeChecker()
         self._embeddingRegistry = EmbeddingRegistry() 
-        self._text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size = 500,
-            chunk_overlap = 100,
-            ) 
-        self._vectorstore = Chroma(
-            persist_directory=self._path,
-            embedding_function=self._embeddingRegistry.get_current_text_model(),
-        )
-    
+        self._text_injustor = self._load_text_injustor(path) 
+
+    def _load_text_injustor(self, path: str):
+        embModel=self._embeddingRegistry.get_current_text_model()
+        if embModel == None:
+            raise ValueError(f"Invalid current text embedding model")
+        return Injustor(path, embModel)
+
     def injust(self, path : str) -> None:
         dataType = self._typeChecker.check(path)
 
         if dataType == FileType.TEXT:
-            with open(path, "r") as f:
-                cleaned_text = clean_text(f.read())
-                splits = self._text_splitter.split_text(text=cleaned_text) 
-                self._vectorstore.add_texts(splits)
+            self._text_injustor.injust(path)
 
 
     def retrieve(self, query : str, k : int) -> list[Document]: 
-        results = self._vectorstore.similarity_search(query=query, k=k)
+        results = self._text_injustor._vectorstore.similarity_search(query=query, k=k)
         return results
